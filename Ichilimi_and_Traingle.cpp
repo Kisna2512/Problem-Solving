57657539218cc442e8fb3a5c3f96b009462a459f
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-08-24 17:58:47 +0530

    Create Ichilimi_and_Traingle.cpp

---
+++ Ichilimi_and_Traingle.cpp
@@ -0,0 +1,27 @@
+// Link:- https://codeforces.com/problemset/problem/1337/A
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  #endif
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+    	int a,b,c,d;
+    	cin>>a>>b>>c>>d;
+    	cout<<b<<" "<<c<<" "<<c<<"\n";
+    }
+
+
+    return 0;
+}
