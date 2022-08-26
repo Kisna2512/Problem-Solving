0522696dee0526b1060976b6b1387aa4d9e58f50
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-08-26 17:57:33 +0530

    Create Mahmoud and Ehab and the even-odd game.cpp

---
+++ Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int n;
+    cin>>n;
+
+    if(n&1)
+    {
+    	cout<<"Ehab"<<endl;
+    }
+    else
+    {
+    	cout<<"Mahmoud"<<endl;
+    }
+
+
+
+
+    return 0;
+}
