e33b1ecba11e9899a52e599d29b9e67561f9aa2f
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-09-29 11:11:29 +0530

    Time: 266 ms (5.00%), Space: 36.9 MB (14.13%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -9,31 +9,15 @@ public:
         }
         return a.first < b.first;
     }
-    
-    
-    
+
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         
         vector<pair<int,int>> ans;
-        priority_queue <pair<int,int> > pq;
-        
-        
         for(int i=0;i<arr.size();i++)
         {
-            // pq.push({abs(arr[i]-x),arr[i]});
             ans.push_back({abs(arr[i]-x),arr[i]});
         }
         sort(ans.begin(),ans.end(),comp);
-        for(auto it:ans)
-        {
-            cout<<it.first<<" "<<it.second<<endl;
-        }
-        // while(!pq.empty())
-        // {
-        //     ans.push_back(pq.top().second);
-        //     pq.pop();
-        // }
-        
         arr.clear();
         int i=0;
         while(k--)
@@ -42,19 +26,7 @@ public:
             i++;
         }
         sort(arr.begin(),arr.end());
-        return arr;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return arr;  
         
     }
 };
