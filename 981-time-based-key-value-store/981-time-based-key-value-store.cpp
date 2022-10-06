28143e6fcb65ca54ff28a33eb8b8427f8ae6938e
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-10-06 14:52:16 +0530

    Time: 679 ms (37.51%), Space: 130.6 MB (36.13%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -13,6 +13,7 @@ public:
     
     string get(string key, int timestamp) {
           auto it = m[key].upper_bound(timestamp);
+          
           return it == m[key].begin() ? "" : prev(it)->second;
     }
 };
