b252b1bce96b18aa89a1461746ebfc829127b91d
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-10-30 17:13:27 +0530

    Time: 100 ms (24.67%), Space: 19.3 MB (11.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,63 +1,66 @@
 class Solution
 {
+
     public:
-        int directions[4][2] = {
-		    {-1,0 },
-            { 1,0 },
-            { 0,-1},
-            { 0,1 }
-        };
-    void bfs(int i, int j, vector<vector < bool>> &vis, vector< vector< char>> &grid)
-    {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int, int>> q;
-        q.push({ i,
-            j });
 
-        vis[i][j] = true;
-        while (!q.empty())
+        void bfs(int row, int col, vector<vector < char>> &grid, vector< vector< int>> &vis)
         {
-            pair<int, int> p = q.front();
-            q.pop();
-            for (auto it: directions)
+            int dx[] = { -1,
+                0,
+                1,
+                0
+            };
+            int dy[] = { 0,
+                1,
+                0,
+                -1
+            };
+            vis[row][col] = 1;
+            queue<pair<int, int>> q;
+            q.push({ row,
+                col });
+
+            while (!q.empty())
             {
-                int row = p.first + it[0];
-                int col = p.second + it[1];
+                int rowii = q.front().first;
+                int colii = q.front().second;
+                q.pop();
 
-                if (row < 0 || col < 0 || row >= n || col >= m || vis[row][col] == true || grid[row][col] == '0')
+                for (int i = 0; i < 4; i++)
                 {
-                    continue;
-                }
-
-                vis[row][col] = true;
-                q.push({ row,
-                    col });
-            }
-        }
-    }
-
-    int numIslands(vector<vector < char>> &grid)
-    {
-
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector < bool>> vis(n, vector<bool> (m, false));
-
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (!vis[i][j] && grid[i][j] == '1')
-                {
-                    cnt++;
-                    bfs(i, j, vis, grid);
+                    int rowi = rowii + dx[i];
+                    int coli = colii + dy[i];
+                    if (rowi >= 0 && coli >= 0 && rowi < grid.size() && coli < grid[0].size() && grid[rowi][coli] == '1' && !vis[rowi][coli])
+                    {
+                        vis[rowi][coli] = 1;
+                        q.push({ rowi,
+                            coli });
+                    }
                 }
             }
+            
+            
         }
+                int numIslands(vector<vector < char>> &grid)
+                {
 
-        return cnt;
-    }
-};
+                    int n = grid.size();
+                    int m = grid[0].size();
+                   	// create visited array and initialise to 0
+                    vector<vector < int>> vis(n, vector<int> (m, 0));
+                    int cnt = 0;
+                    for (int row = 0; row < n; row++)
+                    {
+                        for (int col = 0; col < m; col++)
+                        {
+                           	// if not visited and is a land
+                            if (!vis[row][col] && grid[row][col] == '1')
+                            {
+                                cnt++;
+                                bfs(row, col, grid, vis);
+                            }
+                        }
+                    }
+                    return cnt;
+                }
+            };
