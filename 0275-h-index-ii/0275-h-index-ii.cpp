92da297acda68b3513479c92ae6351f1fd83a6b3
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-11-08 10:08:33 +0530

    Time: 22 ms (85.93%), Space: 18.7 MB (46.70%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int hIndex(vector<int>& nums) {
+        int n = nums.size();
+
+            int low = 0;
+            int high = n - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (nums[mid] == (n - mid))
+                {
+                    return nums[mid];
+                }
+                else if (nums[mid] > (n - mid))
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return n - low;
+    }
+};
