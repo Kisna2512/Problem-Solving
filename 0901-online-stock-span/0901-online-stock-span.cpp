30470ccd919229912a4f974bf08b017c3beac7f4
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-11-09 18:18:22 +0530

    Time: 2850 ms (5.06%), Space: 86.2 MB (15.46%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,53 +1,31 @@
-// class StockSpanner {
-// public:
-    
-//     vector<int> res;
-//     StockSpanner() {
-        
-//     }
-    
-//     int next(int price) {
-//         int ans=0;
-        
-//         res.push_back(price);
-//         for(int i=res.size()-1;i >= 0 ;i--)
-//         {
-//             if(res[i] <= price)
-//             {
-//                 ans++;
-//             }else
-//             {
-//                 break;
-//             }
-//         }
-        
-//         return ans;
-//     }
-// };
-
 class StockSpanner {
 public:
-    vector<int>v;
+    
+    vector<int> res;
     StockSpanner() {
-        v.clear();
+        res.clear();
     }
     
     int next(int price) {
-        int ct = 0;
-        v.push_back(price);
-        for(int i=v.size()-1;i>=0;i--){
-            if(v[i] <= price){
-                ct++;
-                //cout<<v[i]<<" ";
-            }
-            else{
+        int ans=0;
+        
+        res.push_back(price);
+        for(int i=res.size()-1;i >= 0 ;i--)
+        {
+            if(res[i] <= price)
+            {
+                ans++;
+            }else
+            {
                 break;
             }
         }
-        //cout<<endl;
-        return ct;
+        
+        return ans;
     }
 };
+
+
 /**
  * Your StockSpanner object will be instantiated and called as such:
  * StockSpanner* obj = new StockSpanner();
