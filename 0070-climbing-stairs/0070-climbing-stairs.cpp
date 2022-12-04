cc38e81ac68c0086879705115af22b291cd33c17
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-12-04 11:16:05 +0530

    Time: 2 ms (42.81%), Space: 6.3 MB (15.11%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+
+        int solve(int n, vector<int>& dp)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (dp[n] != -1)
+            {
+                return dp[n];
+            }
+
+            return dp[n] = solve(n - 2,dp) + solve(n - 1,dp);
+        }
+
+    int climbStairs(int n)
+    {
+        vector<int> dp(n+1, -1);
+
+        return solve(n, dp);
+    }
+};
