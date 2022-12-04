80d131d9d596b5cfcc398310639d3965300a6f03
Krishna Gajanan Kotgire <90544124+Kisna2512@users.noreply.github.com>
2022-12-04 11:25:05 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (82.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,8 @@
 class Solution
 {
     public:
-
-        int solve(int n, vector<int> &dp)
+       	// Recursive TC:O(2^N) SC:O(N)
+        int solverec(int n)
         {
             if (n == 0)
             {
@@ -14,20 +14,46 @@ class Solution
                 return 1;
             }
 
-            if (dp[n] != -1)
-            {
-                return dp[n];
-            }
-
-            return dp[n] = solve(n - 2, dp) + solve(n - 1, dp);
+            return solverec(n - 1) + solverec(n - 2);
         }
 
-    int climbStairs(int n)
+   	// Memoization TC:O(N) SC:O(N)+O(N)
+    int solveMemo(int n, vector<int> dp)
     {
-       	//         vector<int> dp(n+1, -1);
+        if (n == 0)
+        {
+            return 1;
+        }
+        if (n == 1)
+        {
+            return 1;
+        }
 
-       	//         return solve(n, dp);
+        if (dp[n] != -1)
+        {
+            return dp[n];
+        }
 
+        return dp[n] = solveMemo(n - 1, dp) + solveMemo(n - 2, dp);
+    }
+
+   	// Tabulation TC:O(N) SC:O(N)
+    int solveTab(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+   	// Space Optimization TC:O(N) SC:O(1)
+    int solvespace(int n)
+    {
         int prev2 = 1;
         int prev = 1;
 
@@ -37,7 +63,11 @@ class Solution
             prev2 = prev;
             prev = curr;
         }
-
         return prev;
+    }
+
+    int climbStairs(int n)
+    {
+       	return solvespace(n);
     }
 };
